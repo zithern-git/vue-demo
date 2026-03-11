9a310b7223d59eb54b7568691a2d5c1f654d8ccc
zithern-git <2385186645@qq.com>
2026-03-11 17:43:07 +0800

    新增嵌套路由

---
+++ copy/25_嵌套路由/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>这是首页</h2>
+  </div>
+</template>
+
+<script setup lang="ts" name="'Home'"></script>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
